ebc3ffe3a2437b640961fcb2c4f78490baf18da4
alsahba <alisahinbalikci@gmail.com>
2019-03-28 22:55:00 +0300

    Skeleton Structure v2

--- Main.py
+++ Main.py
@@ -1,23 +1,27 @@
+from Stemmer import Stemmer
+import math
 
-def wordOrganizer(word):
-    word = word.replace("_"," ")
-    if '\'' in word:
-        n = word[0:int(word.find('\''))]
-        return n.lower()
-    return word.lower()
+def calculateTotalNumberOfTwoLayerMapping(mapping):
+    return sum(count for outerScope in mapping.items() for word, count in outerScope[1].items())
 
 
-def splitWordAndTag(word_with_tag):
+def calculateTotalNumberOfOneLayerMapping(mapping):
+    return sum(count for word, count in mapping.items())
+
+
+def splitWordAndTag(stemmer, word_with_tag):
     separation = word_with_tag.split('/')
-    word = wordOrganizer(separation[0])
-    tag = wordOrganizer(separation[1])
+    word = stemmer.wordOrganizer(separation[0])
+    tag = stemmer.wordOrganizer(separation[1])
     return word, tag
 
 
-def countEmission(emission, words_with_tags):
+def countEmission(stemmer, emission, words_with_tags):
     for word_with_tag in words_with_tags:
-        word = splitWordAndTag(word_with_tag)[0]
-        tag = splitWordAndTag(word_with_tag)[1]
+        word = splitWordAndTag(stemmer, word_with_tag)[0]
+        if word == 'not a word':
+            continue
+        tag = splitWordAndTag(stemmer, word_with_tag)[1]
 
         try:
             emission[tag][word] += 1
@@ -28,10 +32,13 @@ def countEmission(emission, words_with_tags):
                 emission[tag] = {word: 1}
 
 
-def countTransition(transition, word_with_tags):
+def countTransition(stemmer, transition, word_with_tags):
     for i in range(len(words_with_tags) - 1):
-        tag = splitWordAndTag(words_with_tags[i])[1]
-        next_tag = splitWordAndTag(words_with_tags[i + 1])[1]
+        tag = splitWordAndTag(stemmer, words_with_tags[i])[1]
+        next_tag = splitWordAndTag(stemmer, words_with_tags[i + 1])[1]
+
+        if tag == 'end' and next_tag == 'start':
+            continue
 
         try:
             transition[tag][next_tag] += 1
@@ -42,15 +49,35 @@ def countTransition(transition, word_with_tags):
                 transition[tag] = {next_tag: 1}
 
 
-f = open("metu.txt", "r")
-emission = {}
-transition = {}
+def calculateProbability(transition, emission, separated_line):
+    mapo = {}
+    for i in range(len(separated_line) - 1):
+        for m in emission:
+            calc = transition.get('start').get(m) / calculateTotalNumberOfOneLayerMapping(transition.get('start'))
+            try:
+                mapo['start'][m] = calc
+            except:
+                mapo['start'] = {m: calc}
 
-words_with_tags = []
-[words_with_tags.append(word.strip()) for line in f.readlines() for word in line.split()]
 
-countEmission(emission, words_with_tags)
-countTransition(transition, words_with_tags)
 
-print(emission.get('verb'))
-print(transition.get('verb'))
+
+emission, transition = {}, {}
+words_with_tags, lines = [], []
+stemmer = Stemmer()
+
+[lines.append('not_a_word/start ' + line + 'not_a_word/end ') for line in open("data.txt", "r").readlines()]
+[words_with_tags.append(word.strip()) for line in lines for word in line.split()]
+
+countEmission(stemmer, emission, words_with_tags)
+countTransition(stemmer, transition, words_with_tags)
+
+deneme = ["Siz", ",", "sonsuza", "dek", "yürüyeceksiniz", "."]
+
+
+print(calculateTotalNumberOfTwoLayerMapping(emission))
+print(calculateTotalNumberOfTwoLayerMapping(transition))
+print(emission.get('start'))
+print(transition.get('punc'))
+
+calculateProbability(transition,emission,deneme)
