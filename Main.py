62dbfa413cd38f9cd6b54d8b948e1be9dc5b9cf6
alsahba <alisahinbalikci@gmail.com>
2019-03-31 18:44:12 +0300

    More Split And Optimization

--- Main.py
+++ Main.py
@@ -15,16 +15,11 @@ def splitWordAndTag(stemmer, word_with_tag):
 def countEmission(stemmer, emission, words_with_tags):
     for word_with_tag in words_with_tags:
         word, tag = splitWordAndTag(stemmer, word_with_tag)
+
         if word == 'not a word':
             continue
 
-        try:
-            emission[tag][word] += 1
-        except KeyError:
-            try:
-                emission[tag][word] = 1
-            except KeyError:
-                emission[tag] = {word: 1}
+        addDictionaryToMap(emission, tag, word)
 
 
 def countTransition(stemmer, transition, word_with_tags):
@@ -35,13 +30,7 @@ def countTransition(stemmer, transition, word_with_tags):
         if tag == 'end' and next_tag == 'start':
             continue
 
-        try:
-            transition[tag][next_tag] += 1
-        except KeyError:
-            try:
-                transition[tag][next_tag] = 1
-            except KeyError:
-                transition[tag] = {next_tag: 1}
+        addDictionaryToMap(transition, tag, next_tag)
 
 
 def findMaxProbabilityAndConvertToTuple(mapping):
@@ -61,49 +50,31 @@ def findMaxProbabilityForLastWord(mapping):
 
 
 def convertDictToTuple(mapping):
-    return max([innerDict for innerDict in mapping.items()])
+    return [innerDict for innerDict in mapping.items()][0]
 
 
 def tracePath(stemmer, separated_line, mapping):
     trace = []
     sizeOfLine = len(separated_line)
-    prev_tag = ""
+    prev_tag = convertDictToTuple(mapping.get('not a word').get('end'))[1]
     for i in reversed(range(sizeOfLine)):
         word, tag = splitWordAndTag(stemmer, separated_line[i])
-
-        if i == sizeOfLine - 1:
-            found_tag, prev_tag = findMaxProbabilityForLastWord(mapping.get(word))
-            trace.append(found_tag)
-
-        else:
-            tt = convertDictToTuple(mapping.get(word).get(prev_tag))
-            trace.append(prev_tag)
-            prev_tag = tt[1]
-
+        trace.append(prev_tag)
+        prev_tag = convertDictToTuple(mapping.get(word).get(prev_tag))[1]
     return trace
 
 
-def findFirstTransitionProbabilities(stemmer, emission, transition, separated_line, word_probability_mapping):
-    word = splitWordAndTag(stemmer, separated_line[0])[0]
-    for tag in emission:
-        try:
-            transition_probability = (transition.get('start').get(tag) + 1) / (
-                        calculateTotalItems(transition.get('start')) + len(transition.get('start')))
-        except:
-            transition_probability = 0
-        try:
-            emission_probability = (emission.get(tag).get(word) + 1) / (
-                        calculateTotalItems(emission.get(tag)) + len(emission.get(tag)))
-        except:
-            emission_probability = 1 / (calculateTotalItems(emission.get(tag)) + len(emission.get(tag)))
-        try:
-            word_probability_mapping[word][tag] = {transition_probability * emission_probability: 'start'}
-        except:
-            word_probability_mapping[word] = {tag: {transition_probability * emission_probability: 'start'}}
+def calculateTransitionProbability(transition, tag, prev_tag):
+    try:
+        transition_probability = (transition.get(prev_tag).get(tag) + 1) \
+                                 / (calculateTotalItems(transition.get(prev_tag)) + len(
+            transition.get(prev_tag)))
+    except:
+        transition_probability = 0
+    return transition_probability
 
 
-def calculateEmissionProbability(emission, tag ,word):
-    emission_probability = 0
+def calculateEmissionProbability(emission, tag, word):
     try:
         emission_probability = (emission.get(tag).get(word) + 1) / (
                     calculateTotalItems(emission.get(tag)) + len(emission.get(tag)))
@@ -113,24 +84,51 @@ def calculateEmissionProbability(emission, tag ,word):
     return emission_probability
 
 
-def calculateTransitionProbability(transition, tag, probability_prev_tag_tuple):
-    transition_probability = 0
-    try:
-        transition_probability = (transition.get(probability_prev_tag_tuple[1]).get(tag) + 1) \
-                                 / (calculateTotalItems(transition.get(probability_prev_tag_tuple[1])) + len(
-            transition.get(probability_prev_tag_tuple[1])))
-    except:
-        transition_probability = 0
-    return transition_probability
+def findFirstTransitionProbabilities(stemmer, emission, transition, separated_line, word_probability_mapping):
+    word = splitWordAndTag(stemmer, separated_line[0])[0]
+    for tag in emission:
+        transition_probability = calculateTransitionProbability(transition, tag, 'start')
+        emission_probability = calculateEmissionProbability(emission, tag, word)
+        addDictionaryToMap(word_probability_mapping, word, tag, transition_probability * emission_probability, 'start')
+
+
+def addDictionaryToMap(mapping, tag, tag_word, probability=None, prev_tag=None):
+    if probability is None and prev_tag is None:
+        try:
+            mapping[tag][tag_word] += 1
+        except KeyError:
+            try:
+                mapping[tag][tag_word] = 1
+            except KeyError:
+                mapping[tag] = {tag_word: 1}
+    else:
+        try:
+            mapping[tag][tag_word] = {probability: prev_tag}
+        except KeyError:
+            mapping[tag] = {tag_word: {probability: prev_tag}}
+
+
+def findLastTransitionProbabilities(transition, prev_word, word_probability_mapping):
+    max_probability = 0
+    for tag in transition:
+        if tag == 'start':
+            continue
+
+        transition_probability = calculateTransitionProbability(transition, 'end', tag)
+        probability_prev_tag_tuple = convertDictToTuple(word_probability_mapping.get(prev_word).get(tag))
+        calculated_probability = probability_prev_tag_tuple[0] * transition_probability
+
+        if max_probability < calculated_probability:
+            max_probability = calculated_probability
+            addDictionaryToMap(word_probability_mapping, 'not a word', 'end', calculated_probability, tag)
 
 
 def viterbi(stemmer, transition, emission, separated_line):
     word_probability_mapping = {}
-    #todo UNDER CONSTRUCTION STAY AWAKE
     findFirstTransitionProbabilities(stemmer, emission, transition, separated_line, word_probability_mapping)
 
     for i in range(len(separated_line) - 1):
-        prev_word, prev_tag = splitWordAndTag(stemmer, separated_line[i])
+        prev_word = splitWordAndTag(stemmer, separated_line[i])[0]
         word = splitWordAndTag(stemmer, separated_line[i + 1])[0]
 
         for tag in emission:
@@ -139,20 +137,19 @@ def viterbi(stemmer, transition, emission, separated_line):
 
             for prev_probabilities in word_probability_mapping.get(prev_word).items():
                 probability_prev_tag_tuple = convertDictToTuple(prev_probabilities[1])
-                transition_probability = calculateTransitionProbability(transition, tag, probability_prev_tag_tuple)
+                transition_probability = calculateTransitionProbability(transition, tag, probability_prev_tag_tuple[1])
+                calculated_probability = probability_prev_tag_tuple[0] * transition_probability * emission_probability
 
                 try:
-                    temp_mapping[tag][probability_prev_tag_tuple[0] * transition_probability * emission_probability] = prev_probabilities[0]
+                    temp_mapping[tag][calculated_probability] = prev_probabilities[0]
                 except:
-                    temp_mapping[tag] = {probability_prev_tag_tuple[0] * transition_probability * emission_probability: prev_probabilities[0]}
+                    temp_mapping[tag] = {calculated_probability: prev_probabilities[0]}
 
             max_probability_prev_tag_tuple = findMaxProbabilityAndConvertToTuple(temp_mapping)
-            try:
-                word_probability_mapping[word][tag] = {max_probability_prev_tag_tuple[0]: max_probability_prev_tag_tuple[1]}
-            except:
-                word_probability_mapping[word] = {tag: {max_probability_prev_tag_tuple[0]: max_probability_prev_tag_tuple[1]}}
+            temp_mapping.clear()
+            addDictionaryToMap(word_probability_mapping, word, tag, max_probability_prev_tag_tuple[0], max_probability_prev_tag_tuple[1])
 
-    #todo end transitionu icinde birseyler yapmak lazim
+    findLastTransitionProbabilities(transition, splitWordAndTag(stemmer, separated_line[-1])[0], word_probability_mapping)
     return tracePath(stemmer, separated_line, word_probability_mapping)
 
 
@@ -160,7 +157,7 @@ emission, transition = {}, {}
 words_with_tags, lines = [], []
 stemmer = Stemmer()
 
-[lines.append('not_a_word/start ' + line + 'not_a_word/end ') for line in open("data.txt", "r").readlines()]
+[lines.append('not_a_word/start ' + line.strip() + ' not_a_word/end') for line in open("data.txt", "r").readlines()]
 [words_with_tags.append(word.strip()) for line in lines for word in line.split()]
 
 countEmission(stemmer, emission, words_with_tags)
@@ -174,10 +171,11 @@ deneme5 = ["Dışarıya/Noun", "bakın/Verb", "./Punc"]
 # mapps = {'noun': {45: "prev_tag"}, 'un': {60: "prev_tag"},'oun': {80: "prev_tag"}}
 # maxdict = {'non':{0: 'non'}}
 
-# mappi = {'a': {'noun': {45: "prev_tag"}}}
+mappi = {'a': {'noun': {45: "prev_tag"}}}
 # z = max([n for m in mapps.values() for n in m.items()])
 
-# print(z)
+
+print(convertDictToTuple(mappi.get('a').get('noun')))
     # print(max(m))
     # for n in m:
     #     if ()
@@ -192,5 +190,5 @@ deneme5 = ["Dışarıya/Noun", "bakın/Verb", "./Punc"]
 # print(emission.get('start'))
 # print(transition.get('punc'))
 
-cc = viterbi(stemmer,transition,emission,deneme5)
+cc = viterbi(stemmer, transition, emission, deneme)
 print(cc[::-1])
