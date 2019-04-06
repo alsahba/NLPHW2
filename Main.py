b916ab486a8e126a2a8e9502dcc8c36eef1c90fc
alsahba <alisahinbalikci@gmail.com>
2019-04-07 00:08:07 +0300

    Code Commented

--- Main.py
+++ Main.py
@@ -1,16 +1,18 @@
 import time, numpy as np
 # from snowballstemmer import stemmer
-
-
 # turkishStemmer = stemmer("turkish")
 
+# NOTE: Commented lines show different ways of calculating probability or counting.
+# They are commented because of highest accuracy found in this way, other ways are not as good as this solution.
 
+# This method used for calculating total number of items in the emission or transition mapping.
 def calculateTotalItems(mapping):
     return sum(count for word, count in mapping.items())
 
 
+# This method used for word's letter and punctuation handling. It cuts the word after cutting mark and converts
+# word's all letters to lowercase.
 def wordOrganizer(word):
-
     word = word.replace("_", " ")
     if '\'' in word:
         word = word[0:int(word.find('\''))]
@@ -21,6 +23,7 @@ def wordOrganizer(word):
     return word.lower()
 
 
+# This method used for separation of word and its tag, returns a tuple.
 def splitWordAndTag(word_with_tag):
     separation = word_with_tag.split('/')
     word = wordOrganizer(separation[0])
@@ -28,6 +31,11 @@ def splitWordAndTag(word_with_tag):
     return word, tag
 
 
+# This method used for counting the repeated number of emissions. Bigram mapping used for storing tag-word tuples.
+# Handle of the start and end tags made with if condition, special word (not a word) did not count as an emission.
+# Two dictionary used for emission mapping, one of them holds tuples like tag: {word1, word2 ...},
+# other of them holds tuples like word1: {tag1, tag2 ...}
+# This two way of storing helps calculation of probability for viterbi algorithm.
 def observeEmission(emission, emission_reversed, words_with_tags):
     for word_with_tag in words_with_tags:
         word, tag = splitWordAndTag(word_with_tag)
@@ -39,6 +47,9 @@ def observeEmission(emission, emission_reversed, words_with_tags):
         addDictionaryToMapDuple(emission_reversed, word, tag)
 
 
+# This method used for counting the repeated number of transitions. Bigram mapping used for storing tag-prev_tag tuples.
+# Handle of the end and start tags made with if condition, end-start tags did not count as an transition.
+# One dictionary structure used for transition mapping, it is like tag: {next_tag1, next_tag2 ...}
 def observeTransition(transition, words_with_tags):
     for index in range(len(words_with_tags) - 1):
         tag = splitWordAndTag(words_with_tags[index])[1]
@@ -50,10 +61,17 @@ def observeTransition(transition, words_with_tags):
         addDictionaryToMapDuple(transition, tag, next_tag)
 
 
+# This method used for calculating max probability for viterbi algorithm.
+# Previous words all tags are tried and put in temporary dictionary structure.
+# With this algorithm max probability found and then algorithm can decide which tag is used for previous word.
+# Returns a tuple that includes max probability and tag.
 def findMaxProbabilityAndConvertToTuple(mapping):
     return max([(inner_dict[1], inner_dict[0]) for outer_dict in mapping.items() for inner_dict in outer_dict[1].items()])
 
 
+# This method used for tracing end to start for accuracy calculation.
+# Decide which tag is followed by end, then iteratively found previous tag that followed by current tag through start.
+# All previous tags added to a list and return that list.
 def tracePath(separated_line, array):
     trace, sizeOfLine = [], len(separated_line)
     prev_tag = array[sizeOfLine].get('end')[0]
@@ -64,6 +82,8 @@ def tracePath(separated_line, array):
     return trace
 
 
+# This method used for calculating transition probability for previous tag to current tag by looking transition bigram mapping.
+# Add one smoothing used for unseen transitions.
 def calcTransitionProbability(transition, tag, prev_tag):
     denominator = calculateTotalItems(transition.get(prev_tag)) + len(transition.get(prev_tag))
     try:
@@ -73,6 +93,9 @@ def calcTransitionProbability(transition, tag, prev_tag):
     return transition_probability
 
 
+# This method used for calculating emission probability for known tag and word but looking emission bigram mapping.
+# Add one smoothing did not use for unseen emissions, purpose of this is higher accuracy maintained by this solution.
+# Another solution with add one smoothing commented in this method.
 def calcEmissionProbability(emission, tag, word):
     denominator = calculateTotalItems(emission.get(tag))
     # denominator = calculateTotalItems(emission.get(tag)) + len(emission.get(tag))
@@ -84,6 +107,7 @@ def calcEmissionProbability(emission, tag, word):
     return emission_probability
 
 
+# This method used for adding elements to probability array.
 def addDictionaryToArray(array, index, tag, prev_tag, probability):
     try:
         array[index][tag] = (prev_tag, probability)
@@ -91,6 +115,11 @@ def addDictionaryToArray(array, index, tag, prev_tag, probability):
         array[index] = {tag: (prev_tag, probability)}
 
 
+# This method used for calculation of first transition from start to first word.
+# There is a handle mechanism used for unseen emission, reason of that is higher accuracy provided by this solution.
+# If word has not been seen before, we look only transition probability,
+# skipped emission probability which is equal to zero.
+# All tag transition probabilities calculated with emission probability and put into the array.
 def calcInitialProbability(emission, emission_reversed, transition, separated_line, word_probability_array):
     word = splitWordAndTag(separated_line[0])[0]
     for tag in emission:
@@ -104,6 +133,8 @@ def calcInitialProbability(emission, emission_reversed, transition, separated_li
         addDictionaryToArray(word_probability_array, 0, tag, 'start', calculated_probability)
 
 
+# This method used for calculation of last transition probability.
+# Maximum probability of last word's tags to 'end' tag probability is calculated and found, then added to array.
 def calcLastProbability(transition, word_probability_array, index):
     max_probability = 0
     for tag in transition:
@@ -119,6 +150,7 @@ def calcLastProbability(transition, word_probability_array, index):
             addDictionaryToArray(word_probability_array, index + 1, 'end', tag, calculated_probability)
 
 
+# This method used for counting and adding elements for emission or transition mappings.
 def addDictionaryToMapDuple(mapping, tag, tag_word):
     try:
         mapping[tag][tag_word] += 1
@@ -129,6 +161,7 @@ def addDictionaryToMapDuple(mapping, tag, tag_word):
             mapping[tag] = {tag_word: 1}
 
 
+# This method used for adding elements to a mapping for viterbi algorithm.
 def addDictionaryToMapTriple(mapping, tag, prev_tag, probability):
     try:
         mapping[tag][prev_tag] = probability
@@ -136,12 +169,23 @@ def addDictionaryToMapTriple(mapping, tag, prev_tag, probability):
         mapping[tag] = {prev_tag: probability}
 
 
+# This method used for comparing viterbi algorithm's result and actual data in one line.
 def compareResults(separated_line, result_list):
     correct_result = sum([1 for index, word_with_tag in enumerate(separated_line)
                           if splitWordAndTag(word_with_tag)[1] == result_list[index]])
     return correct_result, len(separated_line)
 
 
+# Viterbi algorithm implementation.
+# Firstly an array is created. In this solution array-dictionary hybrid structure used.
+# Array part is for words in one line. All words associated with indexes. Last index reserved for 'end' tag.
+# Initial probability calculated and added to map, then iteratively all words flowed for this algorithm.
+# Calculation mechanism is in general previous_probability * transition_probability * emission_probability. But,
+# if word has not been seen before calculation made with previous_probability * transition_probability
+# like calculation of initial probability. The reason for that is higher accuracy.
+# Previous word's all tags are tried for one tag transition and added to temporary map,
+# then maximum probability is found and added to map.
+# In the end array is returned.
 def viterbi(transition, emission, emission_reversed, separated_line):
     word_probability_array = np.empty(len(separated_line) + 1, dtype=dict)
     calcInitialProbability(emission, emission_reversed, transition, separated_line, word_probability_array)
@@ -168,6 +212,8 @@ def viterbi(transition, emission, emission_reversed, separated_line):
     return word_probability_array
 
 
+# This method used for testing the viterbi algorithm implementation with using test data.
+# In the end a tuple that includes correct detection and total number returned.
 def testData(test_lines, transition, emission, emission_reversed):
     correct_false_tuple = (0, 0)
     for line in test_lines:
@@ -179,6 +225,7 @@ def testData(test_lines, transition, emission, emission_reversed):
     return correct_false_tuple
 
 
+# Main method.
 def main():
     start = time.time()
     emission, emission_reversed, transition = {}, {}, {}
